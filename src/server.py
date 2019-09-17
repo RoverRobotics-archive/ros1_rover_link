13224d525a0a80f14172dc0bd9e7da160c942d21
Tyler <tylerhanson921@gmail.com>
2019-09-17 14:04:12 -0500

    added assets

--- src/server.py
+++ src/server.py
@@ -13,24 +13,14 @@ def getPageHTML():
     return send_file("index.html")
 
 
-@app.route("/css/<file:file>")
-def css(file):
-    return send_file(os.path.join("assets", "css", file))
-
-
-@app.route("/js/<file:file>")
-def js(file):
-    return send_file(os.path.join("assets", "js", file))
-
-
 @app.route("/fonts/<file:file>")
 def fonts(file):
-    return send_file(os.path.join("assets", "fonts", file))
+    return send_file(os.path.join("fonts", file))
 
 
 @app.route("/img/<file:file>")
 def fonts(file):
-    return send_file(os.path.join("assets", "img", file))
+    return send_file(os.path.join("img", file))
 
 
 def signal_handler(signal, frame):
