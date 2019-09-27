13b991a980d5387bf39f7db1d3a1a9f1f5bd6676
Tyler <tylerhanson921@gmail.com>
2019-09-27 11:36:33 -0500

    refactored build process

--- src/server.py
+++ src/server.py
@@ -13,14 +13,24 @@ def getPageHTML():
     return send_file("index.html")
 
 
-@app.route("/fonts/<file:file>")
+@app.route("/fonts/<file>")
 def fonts(file):
     return send_file(os.path.join("fonts", file))
 
 
-@app.route("/img/<file:file>")
-def fonts(file):
+@app.route("/img/<file>")
+def img(file):
     return send_file(os.path.join("img", file))
+
+@app.route("/css/<file>")
+def css(file):
+    return send_file(os.path.join("css", file))
+
+@app.route("/js/<file>")
+def js(file):
+    return send_file(os.path.join("js", file))
+
+
 
 
 def signal_handler(signal, frame):
